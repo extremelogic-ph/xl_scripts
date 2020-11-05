99b8d257c60760692780be351e4b6e091af8f1cf
Virgilio Y. So <virgilio.so@leadiro.com>
2020-11-06 06:42:09 +0800

    Feature. Support Not In Branch
    There are cases that you dont want to switch branches to sync codes. In that case we can issue a fetch
    using the branch name and update our master branch

--- xl_sync_git.sh
+++ xl_sync_git.sh
@@ -6,13 +6,24 @@
 
 shopt -s nullglob
 array=(*/)
-shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
+shopt -u nullglob
+
 for i in "${array[@]}"
 do
   if [ -d "$i/.git" ]; then
+    REPO_FOUND=true
     cd $i
     echo "Attempting to sync $i"
-    git pull
+    CURRENT_BRANCH=`git branch --show-current`
+    if [ "master" == "$CURRENT_BRANCH" ]; then
+      git pull
+    else
+      git fetch origin master:master
+    fi
     cd ..
   fi
 done
+
+if [ "true" != "$REPO_FOUND" ]; then
+  echo "No repo in current directory. Nothing updated"
+fi
